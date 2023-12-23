7a6cbb9c58bc20285ad6fa5ae93b1d2d433362e7
Toke Friis <tokefriis@gmail.com>
2023-12-23 13:26:56 +0100

    making the 'choose activity' page

---
+++ lib/pages/choose_activity.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class ChooseActivityScreen extends StatelessWidget {
+  const ChooseActivityScreen({super.key, required this.isTask});
+
+  final bool isTask;
+  final double horizontalPadding = 15;
+
+  @override
+  Widget build(BuildContext context) {
+    double space = 10;
+    int amount = 5;
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              height: 30,
+              child: Placeholder(),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+                  child: Text("Seneste"),
+                ),
+                Container(
+                    color: Colors.red,
+                    child: GridView.count(
+                      shrinkWrap: true,
+                      crossAxisCount: 3,
+                      padding: EdgeInsets.all(10),
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      children: List.generate(3, (index) => ActivityCard()),
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ActivityCard extends StatelessWidget {
+  const ActivityCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[100],
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.grey,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home/widgets/add_activity_fab.dart
+++ lib/pages/home/widgets/add_activity_fab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:productivity_app/pages/choose_activity.dart';
 
 class AddActivitiesFAB extends StatelessWidget {
   const AddActivitiesFAB({super.key});
@@ -13,23 +14,35 @@ class AddActivitiesFAB extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
       children: [
         SpeedDialChild(
-          shape: const CircleBorder(),
-          elevation: 0,
-          labelWidget: const Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: Text("Opgave"),
-          ),
-          child: const Icon(Icons.task_alt),
-        ),
+            shape: const CircleBorder(),
+            elevation: 0,
+            labelWidget: const Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: Text("Opgave"),
+            ),
+            child: const Icon(Icons.task_alt),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const ChooseActivityScreen(isTask: true)));
+            }),
         SpeedDialChild(
-          shape: const CircleBorder(),
-          elevation: 0,
-          labelWidget: const Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: Text("Aktivitet"),
-          ),
-          child: const Icon(Icons.task),
-        ),
+            shape: const CircleBorder(),
+            elevation: 0,
+            labelWidget: const Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: Text("Aktivitet"),
+            ),
+            child: const Icon(Icons.task),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const ChooseActivityScreen(isTask: false)));
+            }),
       ],
     );
   }
