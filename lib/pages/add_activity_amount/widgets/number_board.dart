c1a2f9d9a4fab8fcac94cacbf275c34c0bb8cda1
Toke Friis <tokefriis@gmail.com>
2023-12-24 13:29:30 +0100

    made the number board another color

--- lib/pages/add_activity_amount/widgets/number_board.dart
+++ lib/pages/add_activity_amount/widgets/number_board.dart
@@ -33,62 +33,69 @@ class MyNumberBoard extends StatelessWidget {
       return val == "";
     }
 
-    return GridView.count(
-      childAspectRatio: (2 / 1),
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-      shrinkWrap: true,
-      crossAxisCount: 3,
-      children: List.generate(12, (index) {
-        Widget currentChild;
-        String currentString = index.toString();
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.black
+      ),
+      child: GridView.count(
+        childAspectRatio: (2 / 1),
+        padding: const EdgeInsets.all(8),
+        shrinkWrap: true,
+        crossAxisCount: 3,
+        children: List.generate(12, (index) {
+          Widget currentChild;
+          String currentString = index.toString();
 
-        if (index == 9) {
-          currentString = ",";
-        } else if (index == 10) {
-          currentString = "0";
-        }
-
-        currentChild = index != 11
-            ? Text(currentString)
-            : MaterialButton(
-          onPressed: () {},
-          color: Theme.of(context).primaryColor,
-          child: Text(
-            "Næste",
-            style:
-            TextStyle(color: Theme.of(context).colorScheme.onPrimary),
-          ),
-        );
-
-        return InkWell(
-          // the "næste" button can not be pressed
-          onTap: index != 11
-              ? () {
-            // critera for the board
-            if (!((isStringAComma(currentString) &&
-                doesStringContainComma(typedString)) ||
-                (isStringAZero(currentString) &&
-                    isStringAZero(typedString)) ||
-                (isCommaInThisPosition(typedString, 2)) ||
-                (!isStringAComma(currentString) &&
-                    !doesStringContainComma(typedString) &&
-                    isStringThisLength(typedString, 3)))) {
-              if (isStringAComma(currentString) &&
-                  isStringEmpty(typedString)) {
-                changeTypedString("0,");
-              } else if (isStringAZero(typedString) &&
-                  !isStringAComma(currentString)) {
-                changeTypedString(currentString);
-              } else {
-                changeTypedString("$typedString$currentString");
-              }
-            }
-            ;
+          if (index == 9) {
+            currentString = ",";
+          } else if (index == 10) {
+            currentString = "0";
           }
-              : null,
-          child: Container(alignment: Alignment.center, child: currentChild),
-        );
-      }),
+
+          currentChild = index != 11
+              ? Text(currentString, style: TextStyle(color: Colors.white),)
+              : MaterialButton(
+            onPressed: () {},
+            color: Theme.of(context).primaryColor,
+            child: Text(
+              "Næste",
+              style:
+              TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+            ),
+          );
+
+          return InkWell(
+            // the "næste" button can not be pressed
+            onTap: index != 11
+                ? () {
+              // critera for the board
+              if (!((isStringAComma(currentString) &&
+                  doesStringContainComma(typedString)) ||
+                  (isStringAZero(currentString) &&
+                      isStringAZero(typedString)) ||
+                  (isCommaInThisPosition(typedString, 2)) ||
+                  (!isStringAComma(currentString) &&
+                      !doesStringContainComma(typedString) &&
+                      isStringThisLength(typedString, 3)))) {
+                if (isStringAComma(currentString) &&
+                    isStringEmpty(typedString)) {
+                  changeTypedString("0,");
+                } else if (isStringAZero(typedString) &&
+                    !isStringAComma(currentString)) {
+                  changeTypedString(currentString);
+                } else {
+                  changeTypedString("$typedString$currentString");
+                }
+              }
+              ;
+            }
+                : null,
+            child: Container(alignment: Alignment.center, child: currentChild),
+          );
+        }),
+      ),
     );
   }
 }
