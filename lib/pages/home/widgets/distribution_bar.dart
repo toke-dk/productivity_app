b6ef68bb22dda94b4c454c2bfadff575226a5da1
Toke Friis <tokefriis@gmail.com>
2023-12-28 10:17:54 +0100

    reformat

---
+++ lib/pages/home/widgets/distribution_bar.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../models/activity.dart';
+import '../../../widgets/display_activity_type.dart';
+
+class _ShowActivityTypeColors extends StatelessWidget {
+  const _ShowActivityTypeColors({super.key, required this.typeCounts});
+
+  final Map<ActivityType, int> typeCounts;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+        children: List.generate(typeCounts.length, (index) {
+          final ActivityType currentType = typeCounts.keys.toList()[index];
+          final int currentAmount = typeCounts.values.toList()[index];
+          return Column(
+            children: [
+              Row(
+                children: [
+                  DisplayActivityType(activityType: currentType),
+                  const Spacer(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: 15,
+                        height: 8,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: currentType.color),
+                      ),
+                      Text(currentAmount.toString()),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              )
+            ],
+          );
+        }));
+  }
+}
+
+class DistributionBar extends StatelessWidget {
+  DistributionBar({super.key});
+
+
+  final List<Color> barColors = [
+    Colors.green,
+    Colors.orange,
+    Colors.blue,
+    Colors.purple,
+    Colors.indigo
+  ];
+
+  // makes the percentage list for the gradient
+  List<double> makeStops(Map<ActivityType, double> activityTypeDistribution) {
+    List<double> percentageDuplicateList = [];
+
+    for (var i = 0; i < activityTypeDistribution.length; i++) {
+      double currentPercent = activityTypeDistribution.values.toList()[i];
+      if (i == 0) {
+        percentageDuplicateList.addAll([currentPercent, currentPercent]);
+      } else {
+        double previousPercent = percentageDuplicateList[i - 1];
+        percentageDuplicateList.addAll([
+          previousPercent + currentPercent,
+          previousPercent + currentPercent
+        ]);
+      }
+    }
+    percentageDuplicateList = [
+      0,
+      ...percentageDuplicateList.sublist(0, percentageDuplicateList.length - 1),
+    ];
+    return percentageDuplicateList;
+  }
+
+  // makes the color list for gradients
+  List<Color> makeGradientColors(
+      Map<ActivityType, double> activityTypeDistribution) =>
+      activityTypeDistribution.keys
+          .map((e) => [e.color!, e.color!])
+          .expand((element) => element)
+          .toList();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final List<Activity> activities = Provider.of<ActivityProvider>(context).getAllActivities;
+
+
+    // counts occurrence of each type of activity
+    Map<ActivityType, int> activityTypeCounter = {};
+    for (var activity in activities) {
+      activityTypeCounter[activity.activityType] =
+      !activityTypeCounter.containsKey(activity.activityType)
+          ? (1)
+          : (activityTypeCounter[activity.activityType]! + 1);
+    }
+
+    // gives percentage to each type of activity
+    Map<ActivityType, double> activityTypeDistribution = {};
+    for (var i = 0; i < activityTypeCounter.length; i++) {
+      ActivityType currentActivityType = activityTypeCounter.keys.toList()[i];
+
+      // set the color for the type
+      currentActivityType.color = barColors[i];
+
+      // give the activity type a distribution value
+      activityTypeDistribution[currentActivityType] =
+          activityTypeCounter.values.toList()[i] / activities.length;
+    }
+
+    print("${activityTypeDistribution.keys.map((key) => "act. ${key.name}")}, amount: ${activityTypeDistribution.values.map((val) => "act. ${val}")}");
+
+    return Column(
+      children: [
+        Container(
+          height: 15,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: makeGradientColors(activityTypeDistribution),
+                stops: makeStops(activityTypeDistribution),
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight),
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        _ShowActivityTypeColors(typeCounts: activityTypeCounter)
+      ],
+    );
+  }
+}
--- lib/pages/home/widgets/show_today_overview.dart
+++ lib/pages/home/widgets/show_today_overview.dart
@@ -1,43 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:productivity_app/widgets/display_activity_type.dart';
 import '../../../models/activity.dart';
+import 'distribution_bar.dart';
 
 class ShowTodayOverview extends StatelessWidget {
   ShowTodayOverview({super.key, required this.activities});
 
   final List<Activity> activities;
 
-  final List<Color> barColors = [
-    Colors.green,
-    Colors.orange,
-    Colors.blue,
-    Colors.purple,
-    Colors.indigo
-  ];
-
   @override
   Widget build(BuildContext context) {
-    // counts occurrence of each type of activity
-    Map<ActivityType, int> activityTypeCounter = {};
-    for (var activity in activities) {
-      activityTypeCounter[activity.activityType] =
-          !activityTypeCounter.containsKey(activity.activityType)
-              ? (1)
-              : (activityTypeCounter[activity.activityType]! + 1);
-    }
-
-    // gives percentage to each type of activity
-    Map<ActivityType, double> activityTypeDistribution = {};
-    for (var i = 0; i < activityTypeCounter.length; i++) {
-      ActivityType currentActivityType = activityTypeCounter.keys.toList()[i];
-
-      // set the color for the type
-      currentActivityType.color = barColors[i];
-
-      // give the activity type a distribution value
-      activityTypeDistribution[currentActivityType] =
-          activityTypeCounter.values.toList()[i] / activities.length;
-    }
 
     return Column(
       children: [
@@ -47,69 +19,31 @@ class ShowTodayOverview extends StatelessWidget {
             decoration: BoxDecoration(
                 color: Colors.grey[100],
                 borderRadius: BorderRadius.circular(14)),
-            child: activities.isNotEmpty ? Column(
-              children: [
-                const _QuickStatsText(),
-                const SizedBox(
-                  height: 20,
-                ),
-                _DistributionBar(
-                  activityTypeDistribution: activityTypeDistribution,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                _ShowActivityTypeColors(typeCounts: activityTypeCounter)
-              ],
-            ): Center(child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text("Ingen aktiviteter i dag", style: Theme.of(context).textTheme.bodyLarge,),
-            ),)),
+            child: activities.isNotEmpty
+                ? Column(
+                    children: [
+                      const _QuickStatsText(),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      DistributionBar(
+                      ),
+                    ],
+                  )
+                : Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "Ingen aktiviteter i dag",
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    ),
+                  )),
       ],
     );
   }
 }
 
-class _ShowActivityTypeColors extends StatelessWidget {
-  const _ShowActivityTypeColors({super.key, required this.typeCounts});
-
-  final Map<ActivityType, int> typeCounts;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-        children: List.generate(typeCounts.length, (index) {
-      final ActivityType currentType = typeCounts.keys.toList()[index];
-      final int currentAmount = typeCounts.values.toList()[index];
-      return Column(
-        children: [
-          Row(
-            children: [
-              DisplayActivityType(activityType: currentType),
-              const Spacer(),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Container(
-                    width: 15,
-                    height: 8,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: currentType.color),
-                  ),
-                  Text(currentAmount.toString()),
-                ],
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          )
-        ],
-      );
-    }));
-  }
-}
 
 class _QuickStatsText extends StatelessWidget {
   const _QuickStatsText({super.key});
@@ -159,54 +93,3 @@ class _QuickStatsText extends StatelessWidget {
   }
 }
 
-class _DistributionBar extends StatelessWidget {
-  const _DistributionBar({super.key, required this.activityTypeDistribution});
-
-  final Map<ActivityType, double> activityTypeDistribution;
-
-  // makes the percentage list for the gradient
-  List<double> makeStops(Map<ActivityType, double> activityTypeDistribution) {
-    List<double> percentageDuplicateList = [];
-    for (var i = 0; i < activityTypeDistribution.length; i++) {
-      double currentPercent = activityTypeDistribution.values.toList()[i];
-      if (i == 0) {
-        percentageDuplicateList.addAll([currentPercent, currentPercent]);
-      } else {
-        double previousPercent = percentageDuplicateList[i - 1];
-        percentageDuplicateList.addAll([
-          previousPercent + currentPercent,
-          previousPercent + currentPercent
-        ]);
-      }
-    }
-    percentageDuplicateList = [
-      0,
-      ...percentageDuplicateList.sublist(0, percentageDuplicateList.length - 1),
-    ];
-    return percentageDuplicateList;
-  }
-
-  // makes the color list for gradients
-  List<Color> makeGradientColors(
-          Map<ActivityType, double> activityTypeDistribution) =>
-      activityTypeDistribution.keys
-          .map((e) => [e.color!, e.color!])
-          .expand((element) => element)
-          .toList();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 15,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: makeGradientColors(activityTypeDistribution),
-            stops: makeStops(activityTypeDistribution),
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight),
-        color: Colors.green,
-        borderRadius: BorderRadius.circular(20),
-      ),
-    );
-  }
-}
