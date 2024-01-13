3d43fa647a5244eda366aa89cda0ff129fc0eb7f
Toke Friis <tokefriis@gmail.com>
2024-01-13 12:36:06 +0100

    on add goal button press it navigates to AddGoalPage

--- lib/pages/home/widgets/show_goals.dart
+++ lib/pages/home/widgets/show_goals.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:productivity_app/pages/add_goal/add_goal_page.dart';
 import 'package:productivity_app/widgets/MyThemeButton.dart';
 
 import '../../../models/goal.dart';
 
 class ShowGoalsWidget extends StatelessWidget {
   const ShowGoalsWidget({super.key, required this.goals});
-  
+
   final List<Goal> goals;
 
   @override
@@ -14,16 +15,24 @@ class ShowGoalsWidget extends StatelessWidget {
       width: double.infinity,
       padding: EdgeInsets.all(20),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        color: Colors.grey[100]
-      ),
-      child: goals.isEmpty ? Column(
-        children: [
-          Text("Du har ikke sat et mål endnu"),
-          SizedBox(height: 20,),
-          MyThemeButton(onTap: (){}, labelText: "Angiv mål!",)
-        ],
-      ): Text("Should desplay goal here"),
+          borderRadius: BorderRadius.circular(15), color: Colors.grey[100]),
+      child: goals.isEmpty
+          ? Column(
+              children: [
+                Text("Du har ikke sat et mål endnu"),
+                SizedBox(
+                  height: 20,
+                ),
+                MyThemeButton(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => AddGoalPage()));
+                  },
+                  labelText: "Angiv mål!",
+                )
+              ],
+            )
+          : Text("Should desplay goal here"),
     );
   }
 }
