9bcc7b4182bf7d4fcad67feb82eebd1d7e10df9c
Toke Friis <tokefriis@gmail.com>
2024-01-22 21:44:13 +0100

    now it does not show goals which starts in the future (although it does not indicate it in the ui)

--- lib/shared/extensions/gaol_extensions.dart
+++ lib/shared/extensions/gaol_extensions.dart
@@ -4,19 +4,19 @@ import 'package:productivity_app/shared/extensions/date_time_extensions.dart';
 extension CheckMarkGoalsExtension on List<CheckmarkGoal> {
   List<CheckmarkGoal> get activeGoalsFromToday => this
       .where((element) =>
-          DateTime.now().onlyYearMonthDay.isBefore(element.endDate) ||
-          DateTime.now().onlyYearMonthDay.isAtSameMomentAs(element.endDate))
+          DateTime.now().onlyYearMonthDay.isAfter(element.startDate) &&
+              DateTime.now().onlyYearMonthDay.isBefore(element.endDate) ||
+          DateTime.now().onlyYearMonthDay.isAtSameMomentAs(element.endDate) ||
+          DateTime.now().onlyYearMonthDay.isAtSameMomentAs(element.startDate))
       .toList();
 }
 
 extension AmountGoalsExtension on List<AmountGoal> {
   List<AmountGoal> get activeGoalsFromToday => this
       .where((element) =>
-          DateTime.now()
-              .onlyYearMonthDay
-              .isBefore(element.endDate) ||
-          DateTime.now()
-              .onlyYearMonthDay
-              .isAtSameMomentAs(element.endDate))
+          DateTime.now().onlyYearMonthDay.isAfter(element.startDate) &&
+              DateTime.now().onlyYearMonthDay.isBefore(element.endDate) ||
+          DateTime.now().onlyYearMonthDay.isAtSameMomentAs(element.endDate) ||
+          DateTime.now().onlyYearMonthDay.isAtSameMomentAs(element.startDate))
       .toList();
 }
