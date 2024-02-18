426057748cc3134c7a2717add1e765c5a8042631
Toke Friis <tokefriis@gmail.com>
2024-02-18 14:01:53 +0100

    working on page two on introscreen

--- lib/pages/introduction_screens/page_two.dart
+++ lib/pages/introduction_screens/page_two.dart
@@ -10,13 +10,55 @@ class PageTwoIntroScreen extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Fantastisk!",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Lad os komme i gang",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: myTextField(text: "Kaldenavn *")
+              ),
+              SizedBox(height: 40,),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: myTextField(text: "Fornavn (valgfri)")
+              ),
               SizedBox(height: 20,),
-              Text("Fantastisk!", style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),),
-              Text("Lad os komme i gang", style: Theme.of(context).textTheme.titleMedium,)
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: myTextField(text: "Efternavn (valgfri)")
+              ),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+TextField myTextField({required String text}) {
+  return TextField(
+    textCapitalization: TextCapitalization.words,
+    decoration: InputDecoration(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        labelText: text,
+        filled: true,
+        fillColor: Colors.grey[100],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(100),
+        )),
+  );
 }
