eeb07dbebdd63ca25d8b1d3edf4ca5218636b8bb
Toke Friis <tokefriis@gmail.com>
2024-02-24 00:10:58 +0100

    made som categories

--- lib/pages/add_goal/add_routine.dart
+++ lib/pages/add_goal/add_routine.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:productivity_app/models/goal.dart';
+import 'package:productivity_app/widgets/display_activity_type.dart';
 
 class AddRoutine extends StatelessWidget {
   const AddRoutine(
@@ -10,13 +11,127 @@ class AddRoutine extends StatelessWidget {
   final Function(CheckmarkGoal) onCheckMarkGoalAdd;
   final Function(AmountGoal) onAmountGoalAdd;
 
+  final List<Category> categories = const [
+    Category(
+        name: "Sport", child: Icon(Icons.sports_baseball), color: Colors.green),
+    Category(
+        name: "Kost", child: Icon(Icons.dinner_dining), color: Colors.orange),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tilføj rutine"),
+        title: Text("Vælg kategori"),
       ),
-      body: Center(child: Text("Text")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          itemCount: categories.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              childAspectRatio: 4 / 1),
+          itemBuilder: (BuildContext context, int index) {
+            Category _currentCategory = categories[index];
+            return ShowCategoryWidget(category: _currentCategory);
+          },
+        ),
+      ),
     );
+  }
+}
+
+class ShowCategoryWidget extends StatelessWidget {
+  const ShowCategoryWidget({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: category.color.withOpacity(0.3)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(category.name),
+          CircleAvatar(
+            backgroundColor: category.color.withOpacity(0.8),
+            radius: 13,
+            child: ClipOval(
+              child: Padding(
+                padding: EdgeInsets.all(4),
+                child: FittedBox(
+                    child: ColorFiltered(
+                        colorFilter:
+                            ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                        child: category.child)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Category {
+  final String name;
+  final Widget child;
+  final Color color;
+
+  const Category(
+      {required this.name, required this.child, required this.color});
+}
+
+class DisplayCategory extends StatelessWidget {
+  const DisplayCategory(
+      {super.key,
+      required this.category,
+      this.axisDirection = Axis.horizontal,
+      this.dense = false,
+      this.mainAxisAlignment = MainAxisAlignment.center});
+
+  final Category category;
+  final Axis axisDirection;
+  final bool dense;
+  final MainAxisAlignment mainAxisAlignment;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> childrenToDisplay = [
+      CircleAvatar(
+        radius: 20,
+        child: ClipOval(
+            child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: category.child,
+        )),
+      ),
+      SizedBox(
+        width: axisDirection == Axis.horizontal ? 10 : 0,
+        height: axisDirection == Axis.vertical ? 10 : 0,
+      ),
+      FittedBox(fit: BoxFit.fitWidth, child: Text(category.name))
+    ];
+
+    return axisDirection == Axis.horizontal
+        ? Transform.scale(
+            scale: dense ? 0.8 : 1,
+            child: Row(
+                mainAxisAlignment: mainAxisAlignment,
+                children: childrenToDisplay),
+          )
+        : Transform.scale(
+            scale: dense ? 0.8 : 1,
+            child: Column(
+              mainAxisAlignment: mainAxisAlignment,
+              children: childrenToDisplay,
+            ),
+          );
   }
 }
