6340c65539fb3c1b9178a83731fd98063ef6a844
Toke Friis <tokefriis@gmail.com>
2024-03-24 21:54:15 +0100

    you can now see shimmer effect on donation

--- lib/models/donation.dart
+++ lib/models/donation.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class Donation {
   String name;
@@ -27,28 +30,53 @@ class Donation {
 }
 
 class Medal extends StatelessWidget {
-  const Medal({super.key, required this.bgColor, required this.imageSrc});
+  const Medal(
+      {super.key,
+      required this.bgColor,
+      required this.imageSrc,
+      this.shimmer = false});
 
   final MaterialColor bgColor;
   final String imageSrc;
 
+  final double _radius = 25;
+  final double _border = 2;
+
+  final bool shimmer;
+
   @override
   Widget build(BuildContext context) {
+    final Random _random = Random();
+    final int _delay =
+        _random.nextInt(3500) + 2000; // Random number [2000 : 4499]
+
     return CircleAvatar(
-      radius: 25,
-      backgroundColor: bgColor[100],
-      child: Image.asset(
-        imageSrc,
-        width: 30,
-      ),
-    );
+        radius: _radius + _border,
+        backgroundColor: bgColor == Colors.yellow ? bgColor[800] : bgColor[300],
+        child: Animate(
+            effects: shimmer
+                ? [
+                    ShimmerEffect(
+                        delay: _delay.milliseconds, duration: 1.seconds)
+                  ]
+                : [],
+            onPlay: (controller) => controller.loop(count: 4),
+            child: CircleAvatar(
+              radius: _radius,
+              backgroundColor: bgColor[100],
+              child: Image.asset(
+                imageSrc,
+                width: 30,
+              ),
+            )));
   }
 }
 
 class GoldMedal extends Medal {
   GoldMedal(
       {super.bgColor = Colors.yellow,
-      super.imageSrc = "assets/medals/gold_medal.png"});
+      super.imageSrc = "assets/medals/gold_medal.png",
+      super.shimmer = true});
 }
 
 class SilverMedal extends Medal {
