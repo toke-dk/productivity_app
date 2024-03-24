0041a9b25bae814943a9a740e73fc26fc709ec8e
Toke Friis <tokefriis@gmail.com>
2024-03-24 22:50:12 +0100

    added my own donation with a value of 0.1 kr.

--- lib/shared/all_donations.dart
+++ lib/shared/all_donations.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import '../models/donation.dart';
 
 final List<Donation> kAllDonations = [
-  Donation(name: "Test", valueInKr: 2, dateAdded: DateTime(2024,3,23)),
-  Donation(name: "Test2", valueInKr: 31, message: "Should be bronze", dateAdded: DateTime(2024,3,20)),
-  Donation(name: "Test3", valueInKr: 100, dateAdded: DateTime(2024,3,10)),
-  Donation(name: "Dit navn", valueInKr: 6.9, message: "Din Besked", dateAdded: DateTime(2024, 3, 24)),
-
+  Donation(
+      name: "Toke",
+      valueInKr: 0.1,
+      dateAdded: DateTime(2024, 3, 24),
+      message: "To the moon ^"),
 ];
